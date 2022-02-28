532d7b5449523c9b8e7a65a24b6fda9744d9c12f
Balamegha <bala543ganesh@gmail.com>
2022-02-28 12:02:57 +0530

    this test method

---
+++ ExcelClass.java
@@ -0,0 +1,20 @@
+package airticketfilereader;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ExcelClass {
+
+	
+	@Test
+	public void methodone() {
+		Assert.assertEquals("true", "true");
+	}
+	@Test
+	public void methodtwo() {
+		Assert.assertEquals("true", "false");
+	}
+}
